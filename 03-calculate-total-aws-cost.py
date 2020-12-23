fbe2a13f7c2df9b60a8254544612f37bfcc7d705
David Orman <orman@iastate.edu>
2020-12-23 16:27:24 -0600

    fix

--- 03-calculate-total-aws-cost.py
+++ 03-calculate-total-aws-cost.py
@@ -13,10 +13,8 @@ dbslurm = pymysql.connect(read_default_file=defaults_file)
 
 cursorslurm = dbslurm.cursor()
 sql = "SELECT SUM(Amazonjobcost.origreservedcost),SUM(Amazonjobcost.origspotcost) FROM Amazonjobcost INNER JOIN jobinfo USING (jobid) WHERE jobinfo.enddate >= '" + (datetime.now() - timedelta(days=30)).strftime("%Y-%m-%d") + "' AND jobinfo.enddate <= '" + (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")  + "'"
-print(sql)
 cursorslurm.execute(sql)
 data=cursorslurm.fetchone()
-print(data)
 reserved30=str('${:,.0f}'.format(data[0]/100))
 spot30=str('${:,.0f}'.format(data[1]/100))
 sql = "SELECT SUM(origreservedcost),SUM(origspotcost) FROM Amazonjobcost"
