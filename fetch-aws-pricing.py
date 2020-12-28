1f6e392854d718f548e9443dc7c1273c7c355273
Robert Grandin <grandin.robert@gmail.com>
2020-12-28 16:12:05 -0600

    Add CLI option to fetch script

--- fetch-aws-pricing.py
+++ fetch-aws-pricing.py
@@ -4,9 +4,25 @@ import json
 import requests
 import pymysql
 from os.path import expanduser
+import argparse
+
+# Create an ArgumentParser object to easily handle command-line options and
+# auto-generate helpful CLI help when -h/--help flags are used.
+parser = argparse.ArgumentParser(description='Fetch current AWS pricing for select EC2 instances')
+
+parser.add_argument('--defaults-analysis',
+                    dest='defaults_analysis',
+                    type=str,
+                    help='MySQL connection file for analysis database')
+
+# Parse the arguments provided by the user
+args, leftovers = parser.parse_known_args()
 
 home = expanduser("~")
 defaults_file = home + "/.my.cnf.slurm-aws"
+if args.defaults_analysis is not None:
+    defaults_file = args.defaults_analysis
+    pass
 
 dbcost = pymysql.connect(read_default_file=defaults_file)
 cursorcost = dbcost.cursor()
