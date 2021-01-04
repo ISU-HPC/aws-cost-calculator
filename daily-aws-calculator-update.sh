583286b62a710e2e0034b06e6ce0e90fd6d0a324
Robert Grandin <rgrandin@iastate.edu>
2021-01-04 10:22:01 -0600

    Update script to make customization simpler

--- daily-aws-calculator-update.sh
+++ daily-aws-calculator-update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+script_dir=/home/$USER/aws-cost-calculator
+
 # Import jobs, for last 3 days
-./01-import-job-info.py -d 3
+$script_dir/01-import-job-info.py -d 3
 
 # Fetch current AWS pricing
-./fetch-aws-pricing.py
+$script_dir/fetch-aws-pricing.py
 
 # Calculate AWS costs for newly-imported jobs
-./02-calculate-job-costs.py
+$script_dir/02-calculate-job-costs.py
 
